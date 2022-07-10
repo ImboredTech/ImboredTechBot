76b39dd2e4df7e1c1fd5826baacbbd08563b7042
ImboredTech <106279751+ImboredTech@users.noreply.github.com>
2022-07-10 21:40:22 +0100

    Update bot.py, added custom status

--- bot.py
+++ bot.py
@@ -9,6 +9,7 @@ bot = commands.Bot(command_prefix="!")
 @bot.event
 async def on_ready():
     print("Bot is online.")
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="over ImboredTech"))
 
 
 @bot.command()
