c9b3fa4d675587fa5c5e70ce9c5bf46f7366b2d5
ImboredTech <106279751+ImboredTech@users.noreply.github.com>
2022-07-10 21:50:57 +0100

    Update moderation.py, added kick command.

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -24,6 +24,25 @@ class moderation(commands.Cog):
         except discord.DiscordException:
             await ctx.send(f"The member {member.mention} has not been notified because their dms are closed.")
             await member.ban(reason=reason)
+    
+    @commands.command()
+    @commands.has_guild_permissions(kick_members=True)
+    async def kick(self, ctx, member : discord.Member, *, reason=None):
+        embed = discord.Embed(title="Member Kicked Successfully", color=discord.Color.green())
+        embed.add_field(name="Member Kicked", value=f"{member.mention}", inline=False)
+        embed.add_field(name="Reason", value=reason, inline=False)
+        embed.add_field(name="Date", value=today, inline=False)
+        memberEmbed = discord.Embed(title="You have been kicked!", description=f"Guild: {ctx.guild.name}", color=discord.Color.red())
+        memberEmbed.add_field(name="Staff Member", value=f"{ctx.author.name}", inline=False)
+        memberEmbed.add_field(name="Reason", value=reason, inline=False)
+        memberEmbed.add_field(name="Date", value=today, inline=False)
+        await ctx.send(embed=embed)
+        try:
+            await member.send(embed=memberEmbed)
+            await member.kick()
+        except discord.DiscordException:
+            await ctx.send(f"The member {member.mention} has not been notified because their dms are closed.")
+            await member.kick()
 
 def setup(bot):
     bot.add_cog(moderation(bot))
